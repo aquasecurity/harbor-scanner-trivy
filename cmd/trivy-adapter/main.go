39afcb25421114740338a27a91ef6234dcaa6542
Daniel Pacak <pacak.daniel@gmail.com>
2019-07-16 13:35:00 +0200

    feat: Implement result mapping (#1)

--- cmd/trivy-adapter/main.go
+++ cmd/trivy-adapter/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"github.com/aquasecurity/harbor-trivy-adapter/pkg/etc"
 	v1 "github.com/aquasecurity/harbor-trivy-adapter/pkg/http/api/v1"
 	"github.com/aquasecurity/harbor-trivy-adapter/pkg/image/trivy"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 )
 
-type config struct {
-	addr string
-}
-
 func main() {
-	cfg := getConfig()
+	cfg, err := etc.GetConfig()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 	log.Printf("Starting harbor-trivy-adapter with config %v", cfg)
 
-	scanner, err := trivy.NewScanner()
+	scanner, err := trivy.NewScanner(cfg)
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
@@ -27,21 +26,12 @@ func main() {
 	router := mux.NewRouter()
 	v1Router := router.PathPrefix("/api/v1").Subrouter()
 
+	v1Router.Methods("GET").Path("").HandlerFunc(apiHandler.GetVersion)
 	v1Router.Methods("POST").Path("/scan").HandlerFunc(apiHandler.CreateScan)
 	v1Router.Methods("GET").Path("/scan/{detailsKey}").HandlerFunc(apiHandler.GetScanResult)
 
-	err = http.ListenAndServe(cfg.addr, router)
+	err = http.ListenAndServe(cfg.Addr, router)
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Error: %v", err)
 	}
-}
-
-func getConfig() config {
-	cfg := config{
-		addr: ":8080",
-	}
-	if addr, ok := os.LookupEnv("ADAPTER_ADDR"); ok {
-		cfg.addr = addr
-	}
-	return cfg
 }
