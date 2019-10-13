4ef0c2f4a3e831dc9302908711db76dfc2f6dfe8
Daniel Pacak <pacak.daniel@gmail.com>
2019-10-13 21:55:27 +0200

    test: Add component test for the adapter service
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- go.mod
+++ go.mod
@@ -5,13 +5,16 @@ go 1.13
 require (
 	github.com/Microsoft/hcsshim v0.8.6 // indirect
 	github.com/caarlos0/env/v6 v6.0.0
+	github.com/docker/docker v0.7.3-0.20190506211059-b20a14b54661
 	github.com/gocraft/work v0.5.1
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/mux v1.7.3
+	github.com/opencontainers/go-digest v1.0.0-rc1
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
 	github.com/testcontainers/testcontainers-go v0.0.8
+	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 	gotest.tools v0.0.0-20181223230014-1083505acf35
 )
---
+++ test/component/client_test.go
@@ -0,0 +1,85 @@
+package component
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/aquasecurity/harbor-scanner-trivy/pkg/model/harbor"
+	"net/http"
+	"strings"
+	"time"
+)
+
+// Client is the API client that performs all operations against a Scanner Adapter.
+type Client struct {
+	endpointURL string
+}
+
+// NewClient constructs a new Client with the given Scanner Adapter endpoint URL.
+func NewClient(endpointURL string) *Client {
+	return &Client{
+		endpointURL: strings.TrimRight(endpointURL, "/"),
+	}
+}
+
+// RequestScan sends a ScanRequest to the Scanner Adapter and receives the corresponding ScanResponse.
+// Use the ScanResponse's ID to get the ScanReport or error.
+func (c *Client) RequestScan(request harbor.ScanRequest) (scanResp harbor.ScanResponse, err error) {
+	url := fmt.Sprintf("%s/%s", c.endpointURL, "api/v1/scan")
+	b, err := json.Marshal(request)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(b))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/vnd.scanner.adapter.scan.request+json; version=1.0")
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusAccepted {
+		return scanResp, fmt.Errorf("invalid response status: %v %v", resp.StatusCode, resp.Status)
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&scanResp); err != nil {
+		return
+	}
+
+	return
+}
+
+// GetScanReport polls for ScanReport associated with the given ScanRequest ID.
+func (c *Client) GetScanReport(scanRequestID string) (report harbor.ScanResult, err error) {
+	res, err := c.doGetScanReport(scanRequestID)
+	for err == nil && res.StatusCode == http.StatusFound {
+		time.Sleep(10 * time.Second)
+		res, err = c.doGetScanReport(scanRequestID)
+	}
+	if err != nil {
+		return
+	}
+	if res.StatusCode != http.StatusOK {
+		return report, fmt.Errorf("invalid response status: %s", res.Status)
+	}
+
+	if err = json.NewDecoder(res.Body).Decode(&report); err != nil {
+		return
+	}
+	return
+}
+
+func (c *Client) doGetScanReport(scanRequestID string) (*http.Response, error) {
+	url := fmt.Sprintf("%s/api/v1/scan/%s/report", c.endpointURL, scanRequestID)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0")
+
+	return http.DefaultTransport.RoundTrip(req)
+}
