def748a0fd43010a09f8c9dd63058f32714ce963
Simarpreet Singh <simar@linux.com>
2019-10-17 17:47:01 -0700

    model_test: Add a testcase for invalid url
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- pkg/model/harbor/model_test.go
+++ pkg/model/harbor/model_test.go
@@ -1,17 +1,20 @@
 package harbor
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestScanRequest_GetImageRef(t *testing.T) {
 	testCases := []struct {
-		Request  ScanRequest
-		ImageRef string
+		name          string
+		Request       ScanRequest
+		ImageRef      string
+		expectedError string
 	}{
 		{
+			name: "mongo",
 			Request: ScanRequest{
 				Registry: Registry{
 					URL: "https://core.harbor.domain",
@@ -24,6 +27,7 @@ func TestScanRequest_GetImageRef(t *testing.T) {
 			ImageRef: "core.harbor.domain/library/mongo@test:ABC",
 		},
 		{
+			name: "nginx",
 			Request: ScanRequest{
 				Registry: Registry{
 					URL: "https://core.harbor.domain:443",
@@ -35,6 +39,7 @@ func TestScanRequest_GetImageRef(t *testing.T) {
 			ImageRef: "core.harbor.domain:443/library/nginx@test:DEF",
 		},
 		{
+			name: "harbor",
 			Request: ScanRequest{
 				Registry: Registry{
 					URL: "http://harbor-harbor-registry:5000",
@@ -46,10 +51,24 @@ func TestScanRequest_GetImageRef(t *testing.T) {
 			},
 			ImageRef: "harbor-harbor-registry:5000/scanners/mongo@test:GHI",
 		},
+		{
+			name: "invalid registry url",
+			Request: ScanRequest{
+				Registry: Registry{
+					URL: `"http://foo%bar@www.example.com/"`,
+				},
+			},
+			expectedError: `parsing registry URL: parse "http://foo%bar@www.example.com/": first path segment in URL cannot contain colon`,
+		},
 	}
 	for _, tc := range testCases {
 		imageRef, err := tc.Request.GetImageRef()
-		require.NoError(t, err)
-		assert.Equal(t, tc.ImageRef, imageRef)
+		switch {
+		case tc.expectedError != "":
+			assert.Equal(t, tc.expectedError, err.Error(), tc.name)
+		default:
+			assert.NoError(t, err, tc.name)
+		}
+		assert.Equal(t, tc.ImageRef, imageRef, tc.name)
 	}
 }
