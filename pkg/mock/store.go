5255848234d1e7dcf0ac21f2d4c4cb979a5e831b
knqyf263 <knqyf263@gmail.com>
2023-10-30 14:58:50 +0900

    test: fix args
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/mock/store.go
+++ pkg/mock/store.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"context"
 	"github.com/aquasecurity/harbor-scanner-trivy/pkg/harbor"
 	"github.com/aquasecurity/harbor-scanner-trivy/pkg/job"
 	"github.com/stretchr/testify/mock"
@@ -14,22 +15,22 @@ func NewStore() *Store {
 	return &Store{}
 }
 
-func (s *Store) Create(scanJob job.ScanJob) error {
-	args := s.Called(scanJob)
+func (s *Store) Create(ctx context.Context, scanJob job.ScanJob) error {
+	args := s.Called(ctx, scanJob)
 	return args.Error(0)
 }
 
-func (s *Store) Get(scanJobID string) (*job.ScanJob, error) {
-	args := s.Called(scanJobID)
+func (s *Store) Get(ctx context.Context, scanJobID string) (*job.ScanJob, error) {
+	args := s.Called(ctx, scanJobID)
 	return args.Get(0).(*job.ScanJob), args.Error(1)
 }
 
-func (s *Store) UpdateStatus(scanJobID string, newStatus job.ScanJobStatus, error ...string) error {
-	args := s.Called(scanJobID, newStatus, error)
+func (s *Store) UpdateStatus(ctx context.Context, scanJobID string, newStatus job.ScanJobStatus, error ...string) error {
+	args := s.Called(ctx, scanJobID, newStatus, error)
 	return args.Error(0)
 }
 
-func (s *Store) UpdateReport(scanJobID string, report harbor.ScanReport) error {
-	args := s.Called(scanJobID, report)
+func (s *Store) UpdateReport(ctx context.Context, scanJobID string, report harbor.ScanReport) error {
+	args := s.Called(ctx, scanJobID, report)
 	return args.Error(0)
 }
