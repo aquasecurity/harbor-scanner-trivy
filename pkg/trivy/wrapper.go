ecdcad08960381a63d5e56805007837643aeb537
chenk <hen.keinan@gmail.com>
2024-01-02 10:42:02 +0200

    feat: skip java db (#419)
    * feat: skip java db
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * feat: skip java db
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * test: envvar indentation
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * test: envvar indentation
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    ---------
    
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/trivy/wrapper.go
+++ pkg/trivy/wrapper.go
@@ -134,6 +134,10 @@ func (w *wrapper) prepareScanCmd(imageRef ImageRef, outputFile string) (*exec.Cm
 		args = append([]string{"--skip-db-update"}, args...)
 	}
 
+	if w.config.SkipJavaDBUpdate {
+		args = append([]string{"--skip-java-db-update"}, args...)
+	}
+
 	if w.config.OfflineScan {
 		args = append([]string{"--offline-scan"}, args...)
 	}
